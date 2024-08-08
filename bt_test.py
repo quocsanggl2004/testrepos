580ca169c725afa7f6a4513f06fb6ff11674d7c6
Trần Quốc Sáng <165262879+quocsanggl2004@users.noreply.github.com>
2024-08-08 19:48:17 +0700

    Add files via upload

---
+++ bt_test.py
@@ -0,0 +1,27 @@
+from dslk import*
+
+def main ():
+    ds = DSLienKet()
+    ds.in_ds()
+    #a - them
+    print('a: Them -------------------')
+    so = 12
+    print(f'Them{so}')
+    ds.them(so)
+    ds.in_ds()
+
+    so = 10
+    print
+    #b - chen
+
+    #c - tim
+
+    #d - xoa
+
+    #e - cap nhat 
+
+    #f - xoa het
+#def
+    
+if __name__ == '__main__':
+    main()
